cabd07750dee7205157c7f9dd4e91b1db61ca5b7
Thiago Lourençoni <thiagolrc@gmail.com>
2018-05-21 17:26:43 -0300

    Adds ETag support

--- zuul/README.md
+++ zuul/README.md
@@ -1,5 +1,8 @@
 # Zuul
 Zuul reverse proxy: automatically discover services in eureka and registers URL paths that map each application.
 
-Example: http://localhost:8080/api/app-greeting && http://localhost:8080/api/app-person 
+Example: http://localhost:8080/api/app-greeting && http://localhost:8080/api/app-person
+
+#### ETag
+Refresh the http://localhost:8080/api/app-greeting several times and notice that ever time the response repeat we get an http 304 
 
--- zuul/src/main/java/com/spring/cloud/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/spring/cloud/zuul/ZuulApplication.java
@@ -3,6 +3,10 @@ package com.spring.cloud.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+import javax.servlet.Filter;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -10,5 +14,10 @@ public class ZuulApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulApplication.class, args);
+	}
+
+	@Bean
+	public Filter shallowEtagHeaderFilter() {
+		return new ShallowEtagHeaderFilter();
 	}
 }
